389407c1ffdc0b2b68196383a52fc02a4932c379
kevin120196 <kevintalaverac120196@gmail.com>
2020-10-29 22:23:38 -0600

    Angular Template-driven Forms Part 1

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog, MatDialogRef } from "@angular/material/dialog";
+import { MatFormField } from '@angular/material/form-field';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,9 +8,20 @@ import { MatDialog, MatDialogRef } from "@angular/material/dialog";
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  user={
+    username:"",
+    password:'',
+    remember:false
+  }
+
+  constructor(public dialogRef:MatDialogRef<LoginComponent>) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(){
+    console.log('User:',this.user);
+    this.dialogRef.close();
   }
 
 }
