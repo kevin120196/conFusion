7df8498bd6927f1dd5bb65f217ccf1d37cd207b2
kevin120196 <kevintalaverac120196@gmail.com>
2020-10-29 21:40:40 -0600

    Dialogs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { FlexLayoutModule } from '@angular/flex-layout';
-
+import { MatDialogModule } from "@angular/material/dialog";
 import { AppComponent } from './app.component';
 import 'hammerjs';
 
@@ -23,6 +23,7 @@ import { DishService } from './services/dish.service';
 import { PromotionService } from "./services/promotion.service";
 import { LeaderService } from "./services/leader.service";
 import { AppRoutingModule } from "./app-routing/app-routing.module";
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
@@ -33,7 +34,8 @@ import { AppRoutingModule } from "./app-routing/app-routing.module";
     FooterComponent,
     HomeComponent,
     AboutComponent,
-    ContactComponent
+    ContactComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -44,7 +46,11 @@ import { AppRoutingModule } from "./app-routing/app-routing.module";
     MatListModule,
     MatGridListModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatDialogModule
+  ],
+  entryComponents:[
+    LoginComponent
   ],
   //esta seccion es solo para servicios
   providers: [DishService,PromotionService,LeaderService],
