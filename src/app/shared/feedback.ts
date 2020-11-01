b1361c4374fe62c642608466e6e71e94db52b7dd
kevin120196 <kevintalaverac120196@gmail.com>
2020-10-31 20:30:20 -0600

    Angular Reactive Forms Part 1

---
+++ src/app/shared/feedback.ts
@@ -0,0 +1,12 @@
+export class Feedback{
+    firsname:string;
+    lastname:string;
+    telnum:number;
+    email:string;
+    agree:string;
+    contactype:string;
+    message:string;
+}
+
+export const ContactType = ['None','Tel','Email'];
+
