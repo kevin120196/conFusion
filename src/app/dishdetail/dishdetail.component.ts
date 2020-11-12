3f80583e33ded4195476281d807f11c6c5b4cfe0
kevin120196 <kevintalaverac120196@gmail.com>
2020-11-11 22:16:22 -0600

    Animations Part 1

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -7,11 +7,24 @@ import { switchMap } from "rxjs/operators";
 import { FormBuilder,FormGroup,Validators } from "@angular/forms";
 import { Comment } from "../shared/comment";
 import { invalid } from '@angular/compiler/src/render3/view/util';
-
+import { trigger, state, style, animate,transition } from "@angular/animations";
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
-  styleUrls: ['./dishdetail.component.scss']
+  styleUrls: ['./dishdetail.component.scss'],
+  animations: [
+    trigger('visibility', [
+        state('show', style({
+            transform: 'scale(1.0)',
+            opacity: 1
+        })),
+        state('hidden', style({
+            transform: 'scale(0.5)',
+            opacity: 0
+        })),
+        transition('* => *', animate('0.5s ease-in-out'))
+    ])
+  ]
 })
 export class DishdetailComponent implements OnInit {
     
@@ -24,6 +37,7 @@ export class DishdetailComponent implements OnInit {
   comments:Comment;
   commentForm:FormGroup;
   comment:Comment[]=[];
+  visibility = 'show';
   @ViewChild('cform') commentFormDirective;
 
   formErrors={
@@ -58,10 +72,11 @@ export class DishdetailComponent implements OnInit {
     //const id = this.route.snapshot.params['id'];
 /*     this.dishService.getDish(id)
       .subscribe(dishes => this.dish=dishes); */
-    this.route.params
-      .pipe(switchMap((params:Params) => this.dishService.getDish(params['id'])))
-      .subscribe(dishes => {this.dish=dishes;this.dishcopy=this.dish, this.setPrevNext(dishes.id);},
-      errMsg => this.errMsg = <any>errMsg);
+      
+
+      this.route.params.pipe(switchMap((params: Params) => { this.visibility = 'hidden'; return this.dishService.getDish(params['id']); }))
+      .subscribe(dish => { this.dish = dish; this.dishcopy = dish; this.setPrevNext(dish.id); this.visibility = 'show'; },
+        errmess => this.errMsg = <any>errmess);
   }
 
   onValueChanged(data?:any){
--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -50,7 +50,7 @@ export class DishService {
     //return of(DISHES.filter((dish)=>(dish.id === id))[0]).pipe(delay(2000));
 
     return this.http.get<Dish>(baseURL + 'dishes/' +id)
-      .pipe(catchError(this.proccessHTTPMsgService.handleError));
+      .pipe(catchError(this.proccessHTTPMsgService.handleError),delay(2000));
 
 /*     return new Promise(resolve=>{
       //simulate server latency with 2 second delay
@@ -63,7 +63,7 @@ export class DishService {
     //return of(DISHES.filter((dish) => dish.featured)[0]).pipe(delay(2000));  
 
     return this.http.get<Dish[]>(baseURL + 'dishes?featured=true').pipe(map(dishes => dishes[0]))
-      .pipe(catchError(this.proccessHTTPMsgService.handleError));
+      .pipe(catchError(this.proccessHTTPMsgService.handleError),delay(2000));
     
 /*     return new Promise(resolve=>{
       //simulate server latency with 2 second delay
@@ -75,7 +75,7 @@ export class DishService {
   getDishIds():Observable<string | any>{
     //return of(DISHES.map(dish => dish.id));
     return this.getDishes().pipe(map(dishes=>dishes.map(dish=>dish.id)))
-      .pipe(catchError(error=>error));
+      .pipe(catchError(error=>error),delay(2000));
   }
 
   putDish(dish:Dish):Observable<Dish>{
